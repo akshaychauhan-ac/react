04d302d79714a0838207b091eb986b302363e8c0
Akshay <akshay.chauhan@sap.com>
2021-07-14 14:45:54 +0530

    .

--- projects/launchpad/src/components/list/ListView.jsx
+++ projects/launchpad/src/components/list/ListView.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Header";
 import ListTable from "./ListTable";
 import TableToolbar from "./TableToolbar";
 import tilesList from "../../data/Tiles";
+import axios from "axios";
 
 const ListView = props => {
+  const [users, setUsers] = useState([]);
+
   const launchpadStyle = { backgroundColor: "#e6e9ea" };
   const appDetails = tilesList.filter(app => app.code === props.match.params.id)[0];
+  const tableTitle = appDetails.title + " (" + users.length + ")";
   const toolbarButtons =  [{
     key: 1,
     text: "Create"
@@ -15,11 +19,20 @@ const ListView = props => {
     text: "Delete"
   }];
 
+  async function getUsers () {
+    const data = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+    setUsers(data.data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div style={launchpadStyle}>
       <Header title={appDetails.title}></Header>
-      <TableToolbar title={appDetails.title} buttons={toolbarButtons}></TableToolbar>
-      <ListTable></ListTable>
+      <TableToolbar title={tableTitle} buttons={toolbarButtons}></TableToolbar>
+      <ListTable data={users}></ListTable>
     </div>
   );
 };
