53842aa6bd11e0f12db4266fbdd7814e9dddacad
Akshay <akshay.chauhan@sap.com>
2021-07-15 14:18:09 +0530

    .

--- projects/launchpad/src/components/list/ListTable.jsx
+++ projects/launchpad/src/components/list/ListTable.jsx
@@ -1,87 +1,37 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
+import { Label } from "@ui5/webcomponents-react";
+import { Table, TableColumn, TableRow, TableCell, TableGrowingMode } from "@ui5/webcomponents-react";
 
 const ListTable = props => {
-  const columns = [
-    { id: "name", label: "Name", minWidth: 170 },
-    { id: "username", label: "Username", minWidth: 100 },
-    {
-      id: "phone",
-      label: "Phone",
-      minWidth: 170
-    },
-    {
-      id: "website",
-      label: "Website",
-      minWidth: 170
-    }
-  ];
-  const rows = props.data;
-  
-  const useStyles = makeStyles({
-    root: {
-      width: '100%',
-    },
-    container: {
-      maxHeight: 440,
-    },
-  });
-
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.username}</TableCell>
-                  <TableCell>{row.phone}</TableCell>
-                  <TableCell>{row.website}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination rowsPerPageOptions={[5, 8, 100]} component="div" count={rows.length} rowsPerPage={rowsPerPage} page={page}
-        onPageChange={handleChangePage} onRowsPerPageChange={handleChangeRowsPerPage} />
-    </Paper>
+    <Table growing={TableGrowingMode.Scroll} growingThreshold={"5"} columns={
+      <>
+        <TableColumn>
+          <Label>Name</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>Username</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>Phone</Label>
+        </TableColumn>
+        <TableColumn>
+          <Label>Website</Label>
+        </TableColumn>
+      </>
+    }>
+      {props.data.map((row) => {
+        return (
+          <TableRow role="checkbox" key={row.id}>
+            <TableCell>{row.name}</TableCell>
+            <TableCell>{row.username}</TableCell>
+            <TableCell>{row.phone}</TableCell>
+            <TableCell>{row.website}</TableCell>
+          </TableRow>
+        );
+      })}
+    </Table>
   );
 };
   
--- projects/launchpad/src/components/list/ListView.jsx
+++ projects/launchpad/src/components/list/ListView.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "../Header";
 import ListTable from "./ListTable";
 import { Dialog } from '@ui5/webcomponents-react';
-import { Toolbar, Title, ToolbarSpacer, Button } from "@ui5/webcomponents-react";
+import { Toolbar, Title, ToolbarSpacer, Button, Bar } from "@ui5/webcomponents-react";
 import tilesList from "../../data/Tiles";
 import axios from "axios";
 
@@ -12,14 +12,14 @@ const ListView = props => {
   const launchpadStyle = { backgroundColor: "#e6e9ea" };
   const appDetails = tilesList.filter(app => app.code === props.match.params.id)[0];
   const tableTitle = appDetails.title + " (" + users.length + ")";
-  const toolbarButtons =  [{
-    key: 1,
-    text: "Create",
-    action: "createDialog"
-  }, {
-    key: 2,
-    text: "Delete"
-  }];
+
+  const dialogRef = useRef();
+  const handleOpen = (e) => {
+      dialogRef.current.open();
+  };
+  const handleClose = () => {
+      dialogRef.current.close();
+  };
 
   async function getUsers () {
     const data = await axios.get(`https://jsonplaceholder.typicode.com/users`);
@@ -36,7 +36,14 @@ const ListView = props => {
       <Toolbar>
         <Title level={"H3"} style={{marginLeft: "1rem"}}>{tableTitle}</Title>
         <ToolbarSpacer />
-        {toolbarButtons.map(button => <Button key={button.key} design={"Transparent"}>{button.text}</Button>)}
+        <Button key={"1"} design={"Transparent"} onClick={handleOpen}>{"Create"}</Button>
+        <Button key={"2"} design={"Transparent"} onClick={handleOpen}>{"Delete"}</Button>
+        <Dialog
+          ref={dialogRef}
+          header={<Bar middleContent={<Title level={"H3"}>Create User</Title>}/>}
+          footer={<Bar endContent={<Button onClick={handleClose}>Close</Button>} />}
+        >
+        </Dialog>
       </Toolbar>
       <ListTable data={users}></ListTable>
     </div>
