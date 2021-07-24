6a3d1d0fd09b2a90792878ef6efe3437c2c677ca
Akshay <akshay.chauhan@sap.com>
2021-07-24 18:50:09 +0530

    .

---
+++ projects/shopping-cart/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../logo.svg";
+import { ButtonContainer } from "./Button";
+
+const Bar = styled.header`
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+  @media (max-width: 576px) {
+    .navbar-nav {
+      flex-direction: row !important;
+`;
+const headerStyle = {
+  backgroundColor: "#2a2a72",
+  fontSize: "1.3rem",
+  textTransform: "capitalize",
+  padding: "8px 3rem"
+};
+
+const Header = () => {
+  return (
+    <Bar style={headerStyle} className="navbar">
+      <Link to="/">
+        <img src={logo} alt="store" className="navbar-brand" />
+      </Link>
+      <ul className="navbar-nav align-items-center">
+        <li className="nav-item ml-5">
+          <Link to="/" className="nav-link">
+            products
+          </Link>
+        </li>
+      </ul>
+      <Link to="/cart" className="ml-auto">
+        <ButtonContainer>
+          <span className="mr-2">
+            <i className="fas fa-cart-plus " />
+          </span>
+          my cart
+        </ButtonContainer>
+      </Link>
+    </Bar>
+  );
+}
+
+export default Header;
---
+++ projects/shopping-cart/src/components/ProductList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <h1>Products</h1>
+  );
+}
+
+export default ProductList;
