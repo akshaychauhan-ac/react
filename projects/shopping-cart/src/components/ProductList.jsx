548e3d6bdda9549ba881633cc1eb7190b0fe3aa2
Akshay <akshay.chauhan@sap.com>
2021-07-25 00:35:03 +0530

    .

--- projects/shopping-cart/src/components/ProductList.jsx
+++ projects/shopping-cart/src/components/ProductList.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { storeProducts as products } from "../data";
+import styled from "styled-components";
+import "./ProductList.css";
+
+const ProductWrapper = styled.section`
+  .pb-5, .py-5 {
+    padding-bottom: 3rem!important;
+  }
+  .pt-5, .py-5 {
+    padding-top: 3rem!important;
+  }
+`;
 
 const ProductList = () => {
   return (
-    <h1>Products</h1>
+    <>
+      <ProductWrapper className="py-5">
+        <div className="container">
+          <Title name="our" title="products" />
+          <div className="row">
+            {
+              products.map(product => <Product key={product.id} product={product} />)
+            }
+          </div>
+        </div>
+      </ProductWrapper>
+    </>
   );
 }
 
