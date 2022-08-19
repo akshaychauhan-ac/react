6d72afd3ebd77cab884098662d3c08e0c8055685
Akshay <akshay.chauhan@sap.com>
2022-08-19 15:42:53 +0530

    .

--- projects/div-slider/src/App.js
+++ projects/div-slider/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const div1 = document.getElementById("div1");
+  const div2 = document.getElementById("slider");
+  const div3 = document.getElementById("div3");
+  const div1Event = function (e) {
+    const div1Width = div1.offsetWidth;
+    const div3Width = div3.offsetWidth;
+    const offset = div1Width - e.offsetX;
+    if (offset > 0) {
+      div1.style.width = div1Width - offset + "px";
+      div3.style.width = div3Width + offset + "px";
+    }
+    div1.innerText = div1.offsetWidth;
+    div3.innerText = div3.offsetWidth;
+  };
+
+  const div3Event = function (e) {
+    const div1Width = div1.offsetWidth;
+    const div3Width = div3.offsetWidth;
+    const offset = e.offsetX;
+    div1.style.width = div1Width + offset + "px";
+    div3.style.width = div3Width - offset + "px";
+    div1.innerText = div1.offsetWidth;
+    div3.innerText = div3.offsetWidth;
+  };
+
+  // div1.innerText = div1.offsetWidth + " px";
+  // div3.innerText = div3.offsetWidth + " px";
+
+  document.getElementById("slider").addEventListener("mousedown", (e) => {
+    div1.addEventListener("mousemove", div1Event);
+    div3.addEventListener("mousemove", div3Event);
+  });
+  document.addEventListener("mouseup", function (e) {
+    div1.removeEventListener("mousemove", div1Event);
+    div3.removeEventListener("mousemove", div3Event);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", margin: "50px" }}>
+      <div
+        id="div1"
+        style={{ border: "1px solid", height: "200px", width: "200px" }}
+      ></div>
+      <div
+        id="slider"
+        style={{ border: "2px solid", height: "200px", cursor: "ew-resize" }}
+      ></div>
+      <div
+        id="div2"
+        style={{ border: "1px solid", height: "200px", width: "200px" }}
+      ></div>
     </div>
   );
 }
