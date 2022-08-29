53c7107d6e354541c412cea1c29acfb81c55b6aa
Akshay <akshay.chauhan@sap.com>
2022-08-29 15:16:41 +0530

    .

--- projects/dynamic-table-more-less/src/App.js
+++ projects/dynamic-table-more-less/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  const getUsers = async (iTop) => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    let newData = [];
+
+    for (let i = 0; i < iTop; i++) {
+      newData.push({
+        id: data[i].id,
+        userId: data[i].userId,
+        title: data[i].title.slice(0, 40),
+        body: data[i].body.slice(0, 80),
+      });
+    }
+
+    setPosts(newData);
+  };
+  useEffect(() => {
+    getUsers(10);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts &&
+            posts.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.body}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+      <button
+        onClick={() => {
+          getUsers(showMore ? 10 : 20);
+          setShowMore(!showMore);
+        }}
+      >
+        {!showMore ? "Show More" : "Show Less"}
+      </button>
     </div>
   );
 }
