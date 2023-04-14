c4b3821ed303f5d2a071527a2e434129a5ad7273
Akshay Chauhan <52008891+akshaychauhan-ac@users.noreply.github.com>
2023-04-14 13:27:08 +0530

    .

---
+++ projects/stopwatch/src/App.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    let timeout;
+
+    if (start) {
+      timeout = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [start]);
+
+  return (
+    <div style={{ width: "100%" }}>
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <label>{time}</label>
+      </div>
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        <button
+          onClick={() => {
+            setStart(true);
+          }}
+        >
+          Start
+        </button>
+        <button onClick={() => {
+            setStart(false);
+          }}>Pause</button>
+        <button onClick={() => {
+            setStart(false);
+            setTime(0);
+          }}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
