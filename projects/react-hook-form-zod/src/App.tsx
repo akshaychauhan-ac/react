004806154eb0dc200ef5049eb95180107265e5bf
Akshay Chauhan <52008891+akshaychauhan-ac@users.noreply.github.com>
2024-03-05 12:26:42 +0530

    .

--- projects/react-hook-form-zod/src/App.tsx
+++ projects/react-hook-form-zod/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
-function App() {
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormFields = z.infer<typeof schema>;
+
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    mode: "onBlur",
+    defaultValues: {
+      email: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(data);
+    } catch (error) {
+      setError("root", {
+        message: "This email is already taken",
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="tutorial gap-2" onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} type="text" placeholder="Email" />
+      {errors.email && (
+        <div className="text-red-500">{errors.email.message}</div>
+      )}
+      <input {...register("password")} type="password" placeholder="Password" />
+      {errors.password && (
+        <div className="text-red-500">{errors.password.message}</div>
+      )}
+      <button disabled={isSubmitting} type="submit">
+        {isSubmitting ? "Loading..." : "Submit"}
+      </button>
+      {errors.root && <div className="text-red-500">{errors.root.message}</div>}
+    </form>
   );
-}
+};
 
 export default App;
