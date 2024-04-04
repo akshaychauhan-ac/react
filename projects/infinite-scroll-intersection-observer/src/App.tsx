10ed12ae8570742a97464d1693f6d30a719895e5
Akshay Chauhan <52008891+akshaychauhan-ac@users.noreply.github.com>
2024-04-04 20:56:44 +0530

    .

---
+++ projects/infinite-scroll-intersection-observer/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import InfiniteScrollExample from './InfiniteScrollExample';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <InfiniteScrollExample />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/infinite-scroll-intersection-observer/src/InfiniteScrollExample.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import Loader from "./Loader";
+
+const InfiniteScrollExample = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [index, setIndex] = useState(2);
+  const loaderRef = useRef(null);
+
+  const fetchData = useCallback(async () => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+    axios
+      .get(`https://api.escuelajs.co/api/v1/products?offset=${index}&limit=12`)
+      .then((res) => {
+        setItems((prevItems) => [...prevItems, ...res.data]);
+      })
+      .catch((err) => console.log(err));
+
+    setIndex((prevIndex) => prevIndex + 1);
+
+    setIsLoading(false);
+  }, [index, isLoading]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const target = entries[0];
+      if (target.isIntersecting) {
+        fetchData();
+      }
+    });
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, [fetchData]);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          "https://api.escuelajs.co/api/v1/products?offset=10&limit=12"
+        );
+        setItems(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        {items.map((item, index) => (
+          <ProductCard data={item} key={item.id} />
+        ))}
+      </div>
+      <div ref={loaderRef}>{isLoading && <Loader />}</div>
+    </div>
+  );
+};
+
+export default InfiniteScrollExample;
